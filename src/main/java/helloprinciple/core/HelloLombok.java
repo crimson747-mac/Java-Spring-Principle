442a77908df40af1b4d53a8fa26af14f751f0c36
김남우 <crimson747@naver.com>
2021-05-13 02:13:21 +0900

    롬복과 최신 트렌드

---
+++ src/main/java/helloprinciple/core/HelloLombok.java
@@ -0,0 +1,19 @@
+package helloprinciple.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("Ark");
+        System.out.println("hello Lombok = " + helloLombok.toString());
+    }
+}
--- src/main/java/helloprinciple/core/order/OrderServiceImpl.java
+++ src/main/java/helloprinciple/core/order/OrderServiceImpl.java
@@ -3,21 +3,16 @@ package helloprinciple.core.order;
 import helloprinciple.core.discount.DiscountPolicy;
 import helloprinciple.core.member.Member;
 import helloprinciple.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;;
     private final DiscountPolicy discountPolicy;
-
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
