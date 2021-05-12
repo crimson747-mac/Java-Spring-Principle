f8ca86e1a00f33eb2b6d23f179b9f3fdd5bb3807
김남우 <crimson747@naver.com>
2021-05-12 20:37:54 +0900

    싱글톤 패턴

--- src/test/java/helloprinciple/core/singleton/SingletonTest.java
+++ src/test/java/helloprinciple/core/singleton/SingletonTest.java
@@ -1,2 +1,38 @@
-package helloprinciple.core.singleton;public class SingletonTest {
+package helloprinciple.core.singleton;
+
+import helloprinciple.core.AppConfig;
+import helloprinciple.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        // 1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        // 1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조 값이 다른 것을 확인
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 사용한 객체 사용")
+    void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        // same: ==
+        // equal: equals()
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
 }
