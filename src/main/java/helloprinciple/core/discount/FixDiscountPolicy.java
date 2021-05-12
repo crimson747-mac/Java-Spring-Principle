c8d540293a9b7068bfbc44f503ac4ca13de3a76d
김남우 <crimson747@naver.com>
2021-05-13 02:29:38 +0900

    조회한 빈이 2개 이상 - 문제

--- src/main/java/helloprinciple/core/discount/FixDiscountPolicy.java
+++ src/main/java/helloprinciple/core/discount/FixDiscountPolicy.java
@@ -2,7 +2,9 @@ package helloprinciple.core.discount;
 
 import helloprinciple.core.member.Grade;
 import helloprinciple.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixDiscountPolicy  implements DiscountPolicy{
 
     private int discountFixAmount = 1000;
